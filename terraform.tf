86ed13d0164f1ef91784f053edad1ef76245cd8d
duyl97 <33739713+duyl97@users.noreply.github.com>
2023-05-13 09:24:22 +0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  /* Uncomment this block to use Terraform Cloud for this tutorial
-  cloud {
-      organization = "organization-name"
-      workspaces {
-        name = "learn-terraform-refresh"
-      }
-  }
-  */
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
